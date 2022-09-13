966efbc7119b48449d1c3fc4e2ea538dafb3e040
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-09-13 21:35:28 +0200

    added form info

--- index.js
+++ index.js
@@ -9,29 +9,45 @@ function panel() {
 
 function single() {
   one.style.display = "block";
-  if(choose.style.display == "block"){
+  if (choose.style.display == "block") {
     choose.style.display = "none"
   }
 }
 
 function second() {
   two.style.display = "block";
-if(choose.style.display == "block"){
+  if (choose.style.display == "block") {
     choose.style.display = "none"
   }
 }
 
-function builder(){
-    FB.style.display = "block";
-    if(one.style.display == "block" || two.style.display == "block" ) {
+function builder() {
+  FB.style.display = "block";
+  if (one.style.display == "block" || two.style.display == "block") {
     one.style.display = "none"
     two.style.display = "none"
   }
 }
 
-function Preview(){
-    form.style.display = "block";
-    if(FB.style.display == "block"){
-        FB.style.display = "none"
-    }
+function Preview() {
+  var div = document.getElementById("Company2");
+  var input = document.getElementById("Company");
+
+  var div2 = document.getElementById("name5");
+  var input2 = document.getElementById("name4");
+
+  var div3 = document.getElementById("Adress2");
+  var input3 = document.getElementById("Adress");
+
+  var div4 = document.getElementById("Ceo2");
+  var input4 = document.getElementById("Ceo");
+
+  div.value = input.value;
+  div2.value = input2.value;
+  div3.value = input3.value;
+  div4.value = input4.value;
+  form.style.display = "block";
+  if (FB.style.display == "block") {
+    FB.style.display = "none"
+  }
 }
